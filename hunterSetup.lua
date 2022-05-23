bb4b5a8343d316ce21a3de97a464770bfc8ccce4
ダニ <danielrcavalcante@gmail.com>
2022-05-22 23:08:32 -0300

    getting the active hunter aspect

--- hunterSetup.lua
+++ hunterSetup.lua
@@ -1,22 +1,37 @@
+--TODO transform hunterAspects in a key-value with the respective color of aspect
 local hunterAspects = {
     "Aspect of the Viper",
     "Aspect of the Dragonhawk",
-    "Aspect of the Beast",
-    "Aspect of the Cheetah",
-    "Aspect of the Hawk",
-    "Aspect of the Monkey",
-    "Aspect of the Pack",
-    "Aspect of the Wild"}
+    -- "Aspect of the Beast", --actually idk the utility of show it
+    -- "Aspect of the Cheetah", --actually idk the utility of show it
+    -- "Aspect of the Hawk", --actually idk the utility of show it
+    -- "Aspect of the Monkey", --actually idk the utility of show it
+    -- "Aspect of the Pack", --actually idk the utility of show it
+    -- "Aspect of the Wild" --actually idk the utility of show it
+}
 
 
 function setupAddon(self, event, ...) 
-    print("|c98FB98ff(".."hunterSetup successfully loaded"..")")
+    print("|c0003C426(".."hunterSetup successfully loaded"..")")
     self:RegisterEvent("CURRENT_SPELL_CAST_CHANGED") -- Registering in my frame this event
 end
 
-function filterEvent(self, event, ...)
-    print(UnitAura("player",hunterAspects[1]))
-    print(UnitChannelInfo("player"))
+function receiveRegisteredEvent(self, event, ...)
+    local aspect = getActiveAspect()
+    print(aspect)
+    -- print(UnitAura("player",hunterAspects[1]))
+    -- print(UnitAura("player",hunterAspects[2]))
+    -- print(UnitChannelInfo("player"))
 end
 
+function getActiveAspect()
+    local aspectViper = UnitAura("player",hunterAspects[1])
+    local aspectDragonhawk = UnitAura("player",hunterAspects[2])
 
+    if aspectViper ~= nil then return aspectViper end
+    if aspectDragonhawk ~= nil then return aspectDragonhawk end
+end
+
+function changeWindowColor()
+    
+end
