98597a0434b893af06c0973d7cbc2700a1602378
ダニ <danielrcavalcante@gmail.com>
2022-05-29 00:29:12 -0300

    improving some things

--- hunterSetup.lua
+++ hunterSetup.lua
@@ -1,15 +1,11 @@
-
 local hunterAspects = {
-    ["Aspect of the Viper"]      = "|c003440eb",
-    ["Aspect of the Dragonhawk"] = "|c00969696",
-    -- "Aspect of the Beast",    --actually idk the utility of show it
-    -- "Aspect of the Cheetah",  --actually idk the utility of show it
-    -- "Aspect of the Hawk",     --actually idk the utility of show it
-    -- "Aspect of the Monkey",   --actually idk the utility of show it
-    -- "Aspect of the Pack",     --actually idk the utility of show it
-    -- "Aspect of the Wild"      --actually idk the utility of show it
+    ["Aspect_of_the_Viper"]      = "c003440eb",
+    ["Aspect_of_the_Dragonhawk"] = "c00969696"
 }
 
+function replaceDashWithSpace(string)
+    return (string:gsub("_", " "))
+end
 
 function setupAddon(self, event, ...) 
     print("|c0003C426(".."hunterSetup successfully loaded"..")")
@@ -17,32 +13,17 @@ function setupAddon(self, event, ...)
 end
 
 function receiveRegisteredEvent(self, event, ...)
-    local aspect = getActiveAspect()
-    changeButtonColor(aspect)
+    changeWindowColorByAspect()
 end
 
-function getActiveAspect()
-    local activeAspect = nil
+function changeWindowColorByAspect()
     for aspect, color in pairs(hunterAspects) do
-        if UnitAura("player", aspect) ~= nil then activeAspect = aspect end
+        if UnitAura("player", replaceDashWithSpace(aspect)) ~= nil then
+            changeWindowColor(color)
+        end
     end
-    return activeAspect
 end
 
-function changeButtonColor(aspect)
-    -- TODO Improve this verification, use constants    
-    if aspect == "Aspect of the Viper" then createFrameTexture("Aspect of the Dragonhawk") end
-    if aspect == "Aspect of the Dragonhawk" then createFrameTexture("Aspect of the Viper") end
-end
-
-function createFrameTexture(text)
-    -- TODO Change it for a texture to just indicate buff using
-    local btn = CreateFrame("Button", nil, UIParent, "UIPanelButtonTemplate")
-    btn:SetPoint("TOP")
-    btn:SetSize(150, 40)
-    btn:SetText(text)
-    btn:SetScript("OnClick", function(self, button)
-        CastSpellByName(text and "Aspect of the Viper" or "Aspect of the Dragonhawk", true);
-	    print(text and "Aspect of the Viper" or "Aspect of the Dragonhawk" ..button)
-    end)
+function changeWindowColor(color)
+    
 end
