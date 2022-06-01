2752211780e675da1e8f37af4561090c51b5a2e0
ダニ <danielrcavalcante@gmail.com>
2022-05-31 23:24:24 -0300

    improving some algorithms and starting to create a new function

--- hunterSetup.lua
+++ hunterSetup.lua
@@ -6,20 +6,42 @@ local hunterAspects = {
     ["Aspect_of_the_Pack"]       = {1,1,1, .2}
 }
 
+local eventsToRegister = {
+    "CURRENT_SPELL_CAST_CHANGED", --TODO Use better event to get when the player change aspect
+    "UNIT_SPELLCAST_SENT"
+} 
+
 function replaceDashWithSpace(string)
     return (string:gsub("_", " "))
 end
 
 function setupAddon(self, event, ...) 
     print("|c0003C426(".."hunterSetup successfully loaded"..")")
-    if UnitClass("player") == 'Hunter' then self:RegisterEvent("CURRENT_SPELL_CAST_CHANGED") end -- Registering in my frame this event
+    
+    if UnitClass("player") == 'Hunter' then 
+        registerNecessaryEvents(self)
+    end
+end
+
+function registerNecessaryEvents(self)
+    --TODO there is a bug with this loop and de ocurrence of window colors, resolve it
+    for _, event in pairs(eventsToRegister) do
+        self:RegisterEvent(event)
+    end
 end
 
 function receiveRegisteredEvent(self, event, ...)
-    changeWindowColorByAspect()
+    if event == eventsToRegister[1] then changeWindowColorByActiveAspect() end
+    if event == eventsToRegister[2] then printIfWrongTrackingIsActive() end
+    
 end
 
-function changeWindowColorByAspect()
+function printIfWrongTrackingIsActive()
+    --TODO CREATE THIS FUNCTIONS
+    print(UnitCreatureType("target"))
+end
+
+function changeWindowColorByActiveAspect()
     for aspect, color in pairs(hunterAspects) do
         if UnitAura("player", replaceDashWithSpace(aspect)) ~= nil then
             changeWindowColor(color)
@@ -31,15 +53,14 @@ function changeWindowColorByAspect()
 end
 
 function changeWindowColor(color)
-
+    --TODO GET SCREEN RESOLUTION GetScreenWidth(); TO DEFINE IN SETSIZE
+    --TODO IMPROVE THIS "WINDOW CHANGE COLOR" FOR A ANIMATED TEXT IN MIDDLE OF SCREEN
     background:SetSize(1920, 1080)
     background:SetPoint("CENTER")
-
     background:SetBackdrop({
         bgFile = "Interface\\ChatFrame\\ChatFrameBackground",
         edgeFile = "Interface\\ChatFrame\\ChatFrameBackground",
         edgeSize = 1,
     })
     background:SetBackdropColor(color[1], color[2], color[3], color[4])
-
 end
